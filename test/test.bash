d928704c910d15e91f6a8739b7a5611f41539b4e
Naruse Yoshiaki <zhengyit364@gmail.com>
2025-12-27 19:40:02 +0900

    Fix2

--- test/test.bash
+++ test/test.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# デフォルトでは自分のホームディレクトリを使う
+source /opt/ros/foxy/setup.bash
+
 dir="$HOME"
-# 引数が1つ渡されたら、それをホームディレクトリとして扱う
 [ "$1" != "" ] && dir="$1"
 
 cd "$dir/ros2_ws"
+
 colcon build
-source /opt/ros/foxy/setup.bash  
 
-# launchファイルを test.launch.py に変更
-timeout 15 ros2 launch mypkg talker_listener.launch.py > /tmp/mypkg.log
+source install/setup.bash
 
-# 判定条件をサーボ通信の成功ログに変更
+timeout 15 ros2 launch mypkg talker_listener.launch.py > /tmp/mypkg.log || true
+
 cat /tmp/mypkg.log | grep 'Result: True, Angle: 90.0'
