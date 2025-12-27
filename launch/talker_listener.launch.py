3150c12033a35be3530d291d9521e05caaeba412
Naruse Yoshiaki <zhengyit364@gmail.com>
2025-12-27 19:18:57 +0900

    Update1

--- launch/talker_listener.launch.py
+++ launch/talker_listener.launch.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
+import launch
+import launch_ros.actions
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='mypkg',
-            executable='talker',
-            name='talker',
-            output='screen',
-        ),
-        Node(
-            package='mypkg',
-            executable='listener',
-            name='listener',
-            output='screen',
-        ),
-    ])
+    # サーバー側 (talker.py)
+    talker = launch_ros.actions.Node(
+        package='mypkg',
+        executable='talker.py',
+    )
+    # クライアント側 (listener.py)
+    listener = launch_ros.actions.Node(
+        package='mypkg',
+        executable='listener.py',
+        output='screen'
+    )
 
+    return launch.LaunchDescription([talker, listener])
