897c4feb7462b503eed1fea2fb7ff65a1db16220
Naruse Yoshiaki <zhengyit364@gmail.com>
2025-12-27 19:20:07 +0900

    Update2

--- test/test.bash
+++ test/test.bash
@@ -10,7 +10,7 @@ colcon build
 source "$dir/.bashrc"
 
 # launchファイルを test.launch.py に変更
-timeout 15 ros2 launch mypkg test.launch.py > /tmp/mypkg.log
+timeout 15 ros2 launch mypkg talker_listener.launch.py > /tmp/mypkg.log
 
 # 判定条件をサーボ通信の成功ログに変更
 cat /tmp/mypkg.log | grep 'Result: True, Angle: 90.0'
