48c47a95b5d5ddc32ffe37df014fc1c9122db6f4
Naruse Yoshiaki <zhengyit364@gmail.com>
2025-12-27 22:59:46 +0900

    Commit5

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -7,24 +7,13 @@ from mypkg.srv import SetAngle
 def main():
     rclpy.init()
     node = Node("servo_client")
+
     client = node.create_client(SetAngle, 'set_servo_angle')
 
     while not client.wait_for_service(timeout_sec=1.0):
-        node.get_logger().info('waiting...')
+     node.get_logger().info('waiting for service...')
 
-    req = SetAngle.Request(target_angle=90.0)
+    req = SetAngle.Request()
+    req.target_angle = 90.0
+
     future = client.call_async(req)
-
-    rclpy.spin_until_future_complete(node, future)
-
-    try:
-        response = future.result()
-        node.get_logger().info(f'Success: {response.success}')
-    except Exception as e:
-        node.get_logger().error(f'Service call failed: {e}')
-
-    node.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == "__main__":
-    main()
