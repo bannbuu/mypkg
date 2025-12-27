d58b2258723ddb845fecd09c6dce897888a94296
Naruse Yoshiaki <zhengyit364@gmail.com>
2025-12-27 21:59:46 +0900

    Fix

--- launch/talker_listener.launch.py
+++ launch/talker_listener.launch.py
@@ -4,12 +4,12 @@ import launch
 import launch_ros.actions
 
 def generate_launch_description():
-    # サーバー側 (talker.py)
+    #(talker.py)
     talker = launch_ros.actions.Node(
         package='mypkg',
         executable='talker.py',
     )
-    # クライアント側 (listener.py)
+    #(listener.py)
     listener = launch_ros.actions.Node(
         package='mypkg',
         executable='listener.py',
